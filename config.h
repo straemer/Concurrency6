f8c26f00d31d9bd82c93f903ef504df139646699
Stephen Kraemer <straemer@gmail.com>
2013-11-27 19:47:42 -0500

    moved definition of ConfigParams into config.h

--- config.h
+++ config.h
@@ -1,7 +1,18 @@
 #ifndef CONFIG_H__
 #define CONFIG_H__
 
-struct ConfigParams;
+struct ConfigParms {
+    unsigned int sodaCost;                 // MSRP per bottle
+    unsigned int numStudents;              // number of students to create
+    unsigned int maxPurchases;             // maximum number of bottles a student purchases
+    unsigned int numVendingMachines;       // number of vending machines
+    unsigned int maxStockPerFlavour;       // maximum number of bottles of each flavour stocked
+    unsigned int maxShippedPerFlavour;     // number of bottles of each flavour in a shipment
+    unsigned int timeBetweenShipments;     // length of time between shipment pickup
+    unsigned int parentalDelay;            // length of time between cash deposits
+    unsigned int numCouriers;              // number of couriers in the pool
+};
+
 void processConfigFile(const char *configFile, ConfigParms &cparms);
 
 #endif
