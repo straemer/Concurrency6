6b7cf31256e491b3a5f4df77eaf772550e154567
Stephen Kraemer <straemer@gmail.com>
2013-11-27 21:52:26 -0500

    added unit test for watcard

--- test.cc
+++ test.cc
@@ -3,15 +3,46 @@
 #include "watcard.h"
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 _Task WatCardTester {
 public:
+    WatCardTester(WATCard &card) :
+        m_card(card) {}
 private:
+    void main() {
+        m_card.deposit(50);
+        yield();
+        m_card.deposit(50);
+        yield();
+        m_card.withdraw(50);
+        yield();
+        m_card.deposit(25);
+        yield();
+        m_card.withdraw(50);
+        yield();
+        m_card.withdraw(25);
+    }
+
+    WATCard &m_card;
 };
 
 void testWatCard() {
     WATCard testCard;
+    assert(testCard.getBalance() == 0);
+    {
+        vector<WatCardTester*> testers;
+        testers.reserve(32);
+        for (unsigned i=0; i<32; ++i) {
+            testers.push_back(new WatCardTester(testCard));
+        }
+        for (vector<WatCardTester*>::iterator it=testers.begin();
+             it != testers.end();
+             ++it) {
+            delete *it;
+        }
+    }
     assert(testCard.getBalance() == 0);
 }
 
