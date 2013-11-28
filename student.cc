ad6c8aa317f29ab72aec284e4a1c7716ab81cbef
Yasser Al-Khder <yasser.alkhder@gmail.com>
2013-11-27 23:03:29 -0500

    implemented student

--- student.cc
+++ student.cc
@@ -1,8 +1,41 @@
 #include "student.h"
+#include "printer.h"
+#include "nameserver.h"
+#include "watcardoffice.h"
+#include "MPRNG.h"
 
 Student::Student(Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
-                 unsigned int maxPurchases) {
+                 unsigned int maxPurchases) : prt(prt), nameServer(nameServer), cardOffice(cardOffice), id(id), bottlesPurchased(0) {
+    bottlesToPurchase = g_mprng(1, maxPurchases);   // set bottles to purchase between 1 and maxPurchases
+    favouriteFlavour = (VendingMachine::Flavours)g_mprng(0, 3);   // set random flavour
+    fCard = cardOffice.create(id, 5);   // create a WATCard (returns a FWATCard)
+    prt.print(Printer::Student, id, 'S', (unsigned int)favouriteFlavour, bottlesToPurchase);
+    machine = nameServer.getMachine(id);    // obtain vending machine
+    prt.print(Printer::Student, id, 'V', machine->getId());
 }
 
 void Student::main() {
+    while (bottlesPurchased < bottlesToPurchase) {
+        yield(g_mprng(1, 10));  // yield between 1 and 10
+        try {
+            VendingMachine::Status status = machine->buy(favouriteFlavour, *fcard());   // attempt to buy and store status
+        }
+        retry (Lost) {
+            fCard = cardOffice.create(id, 5);
+        }
+        switch (status) {
+            case VendingMachine::BUY:
+                prt.print(Printer::Student, id, 'B', fcard->getBalance());
+                bottlesPurchased++;     // increment bottles purchased
+                break;
+            case VendingMachine::STOCK:
+                machine = nameServer.getMachine(id);     // get new machine
+                prt.print(Printer::Student, id, 'V', machine->getId());
+                break;
+            case VendingMachine::FUNDS:
+                fCard = cardOffice.transfer(id, machine->cost + 5, fcard());  // transfer 5 + cost to watcard
+                break;
+        }
+    }
+    prt.print(Printer::Student, id, 'F');
 }
--- student.h
+++ student.h
@@ -1,14 +1,28 @@
 #ifndef STUDENT_H__
 #define STUDENT_H__
 
+#include "watcard.h"
+#include "vendingmachine.h"
+
 _Monitor Printer;
 _Task NameServer;
 _Task WATCardOffice;
+_Task VendingMachine;
 
 _Task Student {
 public:
     Student(Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
             unsigned int maxPurchases);
+
+    Printer &prt;
+    NameServer &nameServer;
+    WATCardOffice &cardOffice;
+    WATCard::FWATCard fCard;
+    VendingMachine *machine;
+    unsigned int id;
+    unsigned int bottlesPurchased;
+    unsigned int bottlesToPurchase;
+    VendingMachine::Flavours favouriteFlavour;
 private:
     void main();
 };
