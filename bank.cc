95cc59f761a603c43d2769fe9b077841ed4d998e
Stephen Kraemer <straemer@gmail.com>
2013-11-27 22:20:32 -0500

    bank implementation

--- bank.cc
+++ bank.cc
@@ -1,10 +1,24 @@
 #include "bank.h"
 
-Bank::Bank(unsigned int numStudents) {
-}
+Bank::Bank(unsigned int numStudents) :
+    m_balances(numStudents, 0),
+    m_withdrawWaiters(numStudents) {}
 
 void Bank::deposit(unsigned int id, unsigned int amount) {
+    m_balances[id] += amount;
+    m_withdrawWaiters[id].signal();
 }
 
 void Bank::withdraw(unsigned int id, unsigned int amount) {
+    if (!m_withdrawWaiters[id].empty()) {
+        m_withdrawWaiters[id].wait();
+    }
+    while (amount > m_balances[id]) {
+        m_withdrawWaiters[id].wait();
+    }
+    assert(m_balances[id] >= amount);
+    m_balances[id] -= amount;
+
+    // Allow anyone that was waiting on this withdrawal to go now.
+    m_withdrawWaiters[id].signal();
 }
--- bank.h
+++ bank.h
@@ -1,11 +1,16 @@
 #ifndef BANK_H__
 #define BANK_H__
 
+#include <vector>
+
 _Monitor Bank {
 public:
     Bank(unsigned int numStudents);
     void deposit(unsigned int id, unsigned int amount);
     void withdraw(unsigned int id, unsigned int amount);
+private:
+    std::vector<unsigned> m_balances;
+    std::vector<uCondition> m_withdrawWaiters;
 };
 
 #endif
