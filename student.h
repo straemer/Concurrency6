8b244fda1eaeeb151486ed0b0d57eb4d46c6e3ef
Yasser Al-Khder <yasser.alkhder@gmail.com>
2013-11-29 11:48:04 -0500

    Moved members in Student to private

--- student.h
+++ student.h
@@ -13,7 +13,7 @@ _Task Student {
 public:
     Student(Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
             unsigned int maxPurchases);
-
+private:
     Printer &prt;
     NameServer &nameServer;
     WATCardOffice &cardOffice;
@@ -23,7 +23,7 @@ public:
     unsigned int bottlesPurchased;
     unsigned int bottlesToPurchase;
     VendingMachine::Flavours favouriteFlavour;
-private:
+    
     void main();
 };
 
