3f324c5aece9df22a4a98193ba203b9d577c1624
Stephen Kraemer <straemer@gmail.com>
2013-11-29 16:08:59 -0500

    partial watcardoffice implementation

--- watcardoffice.cc
+++ watcardoffice.cc
@@ -1,16 +1,60 @@
 #include "watcardoffice.h"
 
-WATCardOffice::WATCardOffice(Printer &prt, Bank &bank, unsigned int numCouriers) {
+using namespace std;
+
+WATCardOffice::WATCardOffice(Printer &prt, Bank &bank, unsigned int numCouriers) :
+    m_printer(prt),
+    m_bank(bank),
+    m_couriers(numCouriers)
+{
+    for (vector<Courier*>::iterator it = m_couriers.begin();
+         it != m_couriers.end();
+         ++it) {
+        *it = new Courier(*this);
+    }
+}
+
+WATCardOffice::~WATCardOffice() {
+    for (vector<Courier*>::iterator it = m_couriers.begin();
+         it != m_couriers.end();
+         ++it) {
+        delete *it;
+    }
 }
 
 WATCard::FWATCard WATCardOffice::create(unsigned int sid, unsigned int amount) {
+    m_jobs.push(new Job(m_bank, sid, amount));
+    return m_jobs.back()->result;
 }
 
 WATCard::FWATCard WATCardOffice::transfer(unsigned int sid, unsigned int amount, WATCard *card) {
+    m_jobs.push(new Job(m_bank, sid, amount, card));
+    return m_jobs.back()->result;
 }
 
 WATCardOffice::Job *WATCardOffice::requestWork() {
+    while (m_jobs.empty()) {
+        _Accept(create, transfer);
+    }
+    Job *ret = m_jobs.front();
+    m_jobs.pop();
+    return ret;
 }
 
 void WATCardOffice::main() {
+}
+
+void WATCardOffice::Courier::main() {
+    for (;;) {
+        Job *job = m_office.requestWork();
+        job->bank.withdraw(job->studentId, job->amount);
+        WATCard *card = job->card;
+        if (card == NULL) {
+            card = new WATCard;
+            //TODO: need to delete these guys somewhere.
+        }
+        card->deposit(job->amount);
+        job->result.delivery(card);
+        delete job;
+    }
 }
--- watcardoffice.h
+++ watcardoffice.h
@@ -3,25 +3,49 @@
 
 #include "watcard.h"
 
+#include <vector>
+
 _Monitor Printer;
 _Monitor Bank;
 
 _Task WATCardOffice {
 private:
-    struct Job {                           // marshalled arguments and return future
-        WATCard::FWATCard result;                   // return future
-        Job() {}
+    struct Job {
+        WATCard::FWATCard result;
+        Bank &bank;
+        unsigned studentId;
+        unsigned amount;
+        WATCard *card;
+        Job(Bank &bank, unsigned studentId, unsigned amount, WATCard *card = NULL) :
+            bank(bank),
+            studentId(studentId),
+            amount(amount),
+            card(card) {}
     };
 public:
     _Event Lost {};                        // uC++ exception type, like "struct"
     WATCardOffice(Printer &prt, Bank &bank, unsigned int numCouriers);
+    virtual ~WATCardOffice();
     WATCard::FWATCard create(unsigned int sid, unsigned int amount);
     WATCard::FWATCard transfer(unsigned int sid, unsigned int amount, WATCard *card);
     Job *requestWork();
 private:
-    _Task Courier {};                 // communicates with bank
+    _Task Courier {
+    public:
+        Courier(WATCardOffice &office) : m_office(office) {}
+    private:
+        void main();
+
+        WATCardOffice &m_office;
+    };                 // communicates with bank
 
     void main();
+
+    std::queue<Job*> m_jobs;
+    Printer &m_printer;
+    Bank &m_bank;
+    std::vector<Courier*>m_couriers;
+    const unsigned m_numCouriers;
 };
 
 #endif
