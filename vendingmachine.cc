579cd62c4295c176860050e33f4a582b44c065fe
Stephen Kraemer <straemer@gmail.com>
2013-11-29 14:59:46 -0500

    vending machine implementation

--- vendingmachine.cc
+++ vendingmachine.cc
@@ -1,23 +1,44 @@
 #include "vendingmachine.h"
 
+#include "watcard.h"
+
 VendingMachine::VendingMachine(Printer &prt, NameServer &nameServer, unsigned int id,
-                               unsigned int sodaCost, unsigned int maxStockPerFlavour) {
+                               unsigned int sodaCost, unsigned int maxStockPerFlavour) :
+    m_printer(prt),
+    m_nameServer(nameServer),
+    m_id(id),
+    m_cost(sodaCost),
+    m_restocking(false)
+{
+    for (unsigned i=0; i<4; ++i) {
+        m_inventory[i] = 0;
+    }
 }
 
 VendingMachine::Status VendingMachine::buy(Flavours flavour, WATCard &card) {
+    assert((unsigned)flavour < 4);
+    if (m_inventory[flavour] == 0) {
+        return STOCK;
+    }
+    if (card.getBalance() < m_cost) {
+        return FUNDS;
+    }
+    card.withdraw(m_cost);
+    --m_inventory[flavour];
+    return BUY;
 }
 
 unsigned int *VendingMachine::inventory() {
+    m_restocking = true;
+    return m_inventory;
 }
 
 void VendingMachine::restocked() {
-}
-
-unsigned int VendingMachine::cost() {
-}
-
-unsigned int VendingMachine::getId() {
+    m_restocking = false;
 }
 
 void VendingMachine::main() {
+    _When (m_restocking) _Accept(restocked) {
+    } _Else _Accept(buy, inventory) {
+    }
 }
--- vendingmachine.h
+++ vendingmachine.h
@@ -16,12 +16,22 @@ public:
     VendingMachine(Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                    unsigned int maxStockPerFlavour);
     Status buy(Flavours flavour, WATCard &card);
+    // inventory - return the inventory of the vending machine, and prevent buy() from being called
+    // Pretty much the worst API ever.
     unsigned int *inventory();
     void restocked();
-    _Nomutex unsigned int cost();
-    _Nomutex unsigned int getId();
+    inline _Nomutex unsigned int cost() { return m_cost; }
+    inline _Nomutex unsigned int getId() { return m_id; }
 private:
     void main();
+
+    unsigned m_inventory[4];
+
+    Printer &m_printer;
+    NameServer &m_nameServer;
+    const unsigned m_id;
+    const unsigned m_cost;
+    bool m_restocking;
 };
 
 #endif
